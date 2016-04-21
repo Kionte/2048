09119d1dbfc1ed00c0f56cf620a255220b763dd3
VickySorokina <tikivicky@gmail.com>
2016-04-21 13:47:54 -0700

    fixed bugs in initial 1d trial

--- OneDimensionalAmateurVer.cpp
+++ OneDimensionalAmateurVer.cpp
@@ -4,8 +4,8 @@ using namespace std;
 bool AllZeros(const int*, int); //the function that checks if maybe all values in the array are 0 so it could save some thinking time
 
 int main(){
-	const int size = 4; //size of the array (in the future, the size of the side of the "box"
-	int arr[size] = { 2, 4, 0, 2 }; //edit this for check
+	const int size = 5; //size of the array (in the future, the size of the side of the "box"
+	int arr[size] = { 2, 2, 2,4, 2}; //edit this for check
 	char command = ' '; //command needed for inputting a command
 	/*Let's check if there's a way to not have to press enter every single time*/
 
@@ -20,12 +20,11 @@ int main(){
 
 		if (command == 'a'){
 			if (!AllZeros(arr, size)){ //if all values are zeroes there's no point to bother with this line
-				bool fusionHappened = false; //there can only be one "fusion" per line per command so 2 2 4 0 would become 4 4 0 0 not 8 0 0 0
-				
-				for (int i = 0; i < size - 1; i++){ //for every value - 1 because can't collapse last value anyways
 
+				for (int i = 0; i < size - 1; i++){ //for every value - 1 because can't collapse last value anyways
+					int n = 1;
 					if (arr[i] == 0){ //first the collapse. if the value is 0
-						int n = 1;
+						n = 1;
 						while (arr[i] == 0 && n < (size - i)){ //it will search for the next non-zero value
 							if (arr[i + n] != 0){
 								arr[i] = arr[i + n]; arr[i + n] = 0; //assign next non-zero value to current, turn that position 0
@@ -34,20 +33,27 @@ int main(){
 								n++; //if next value also 0 keep looking further
 						}
 					}
-					if (arr[i] != 0 && !fusionHappened){ //unless somewhere something has already added up and these are 0s
-						if (arr[i] == arr[i + 1]){ //if the next value is the sane
-							arr[i] = 2 * arr[i]; arr[i + 1] = 0; //current value doubles, next value turns 0 to be collapsed at next iteration
-							fusionHappened = true; //fusion has happened once
+					n = 1;
+					bool quitLoop = false;
+					while (!quitLoop && n < (size - i)){ //it will search for the next non-zero value
+						if (arr[i + n] == arr[i]){ //if next value same
+							arr[i] = 2 * arr[i]; arr[i + n] = 0; //double current value, turn next one 0
+							quitLoop = true;
+						}
+						else if (arr[i + n] == 0)
+							n++; //if next value 0 keep looking further
+						else
+							quitLoop = true; //if next value different, quit
 						}
 					}
 				}
-			}
+			
+			for (int i = 0; i < size; i++) //print new string
+				cout << arr[i] << " ";
+			cout << endl;
+			cout << "Enter a command, master: "; //ask for new command rinse and repeat
+			cin >> command;
 		}
-		for (int i = 0; i < size; i++) //print new string
-			cout << arr[i] << " ";
-		cout << endl;
-		cout << "Enter a command, master: "; //ask for new command rinse and repeat
-		cin >> command;
 	}
 
 	return 0;
