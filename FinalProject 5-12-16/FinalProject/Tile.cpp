2e4c5b5ae60dcada5850b38b4e78434a19611103
JoelSchroeder <thejoelschroeder@gmail.com>
2016-05-16 16:27:54 -0700

    Add files via upload
    added tile class but not implemented yet. made arrow keys work with previous code.

---
+++ FinalProject 5-12-16/FinalProject/Tile.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include "Tile.h"
+#include <ctime>
+using namespace std;
+
+Tile::Tile()//defualt constrcuctor
+{
+	srand(time(0));
+	int x = rand() % 20;//sets the value to either 2 or 4. there is a one in 20 chance to get a 4
+	if (x != 19) {
+		value = 2;
+	}
+	else {
+		value = 4;
+	}
+	digits = 1;
+	data = makeData();
+}
+
+Tile::Tile(int num)//creates a tile witht he sepcified value
+{
+	value = num;
+	digits = numDigits();
+	data = makeData();
+}
+
+int Tile::getValue() const
+{
+	return value;
+}
+
+string Tile::getData() const
+{
+	return data;
+}
+
+void Tile::changeValue(int num)
+{
+	value = num;
+	digits = numDigits();
+	data = makeData();
+}
+
+int Tile::numDigits()//returns the number of digits in value;
+{
+	string temp = to_string(value);//determines the number of digits in value
+	return temp.length();
+}
+
+string Tile::makeData()//creates the apropriate string based on the number of digits and the value
+{
+	string temp;
+	if (value == 0)
+	{
+		temp = "[     ]";
+	}
+	else
+	{
+		switch (digits)
+		{
+		case 1:
+			temp = "[  ";
+			temp += to_string(value);
+			temp += "  ]";
+			break;
+		case 2:
+			temp = "[  ";
+			temp += to_string(value);
+			temp += " ]";
+			break;
+		case 3:
+			temp = "[ ";
+			temp += to_string(value);
+			temp += " ]";
+			break;
+		case 4:
+			temp = "[ ";
+			temp += to_string(value);
+			temp += "]";
+			break;
+		case 5:
+			temp = "]";
+			temp += to_string(value);
+			temp += "[";
+			break;
+		default:
+			temp = "[ERROR]";
+			break;
+
+		}
+	}
+	return temp;
+}
---
+++ FinalProject 5-12-16/FinalProject/Tile.h
@@ -0,0 +1,33 @@
+//
+//  Tile.h
+//  2048_2.0
+//
+//  Created by Joel Schroeder on 5/7/16.
+//  Copyright © 2016 Joel Schroeder. All rights reserved.
+//
+
+#ifndef TILE_H_
+#define TILE_H_
+#include <string>
+#include <iostream>
+using namespace std;
+class Tile {
+
+private:
+	int value;
+	int digits;
+	string data;
+public:
+	Tile();//defualt constrcuctor
+	Tile(int num);//creates a tile witht he sepcified value
+	int getValue() const;//returns tyhe value
+	string getData() const;//will store a string that represents the tile with correct formatting
+	void changeValue(int num);//will change the value of the tile
+	int numDigits(); //returns the number of digits in value
+	string makeData();//creates the apropriate string based on the number of digits and the value
+	
+};
+
+#endif 
+
+
