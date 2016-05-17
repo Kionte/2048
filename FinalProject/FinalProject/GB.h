120e3851655988261b58bfc00393598dfc93b9f8
JoelSchroeder <thejoelschroeder@gmail.com>
2016-05-16 17:04:34 -0700

    Tile class implemented.
    Tile class implemented, tiles stay same size. score works.

---
+++ FinalProject/FinalProject/GB.h
@@ -0,0 +1,49 @@
+//
+//  GB.h
+//  2048_2.0
+//
+//  Created by Kionte Brown on 5/7/16.
+//  Copyright © 2016 Kionte Brown. All rights reserved.
+//
+
+#ifndef GB_h
+#define GB_h
+#include "Tile.h"
+class GB {
+
+private:
+	int size;
+	int score;
+	Tile** GBValues;
+	int** tempGBValues;
+public:
+	int getScore() const;
+	Tile** getGBValues();
+	int** getTempGBValues() const;
+	int getSize() const;
+
+	int command(); // input move and return move
+	//int setCommand(int newCommand); // main
+	void changeGB(); // switch statement call one o next 4 functions
+
+	bool checkMove();
+	bool gameOverCheck();
+
+	GB(); // intit gameboard
+	GB(int** savedBoard, int savedScore, int size); // load game
+													// ~GB();
+
+	void initGB();
+	void initTempGB();
+	void updateTempGB();
+	void outputGB();
+
+	void leftGB();
+	void rightGB();
+	void upGB();
+	void downGB();
+	void randomInput();
+};
+
+#endif /* GB_h */
+
---
+++ FinalProject/FinalProject/Source.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  2048_2.0
+//
+//  Created by Kionte Brown on 5/7/16.
+//  Copyright © 2016 Kionte Brown. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include "GB.h"
+
+
+void playGame() {
+	//int move;
+	/*cout << "Do you want to load your save? (y/n)";
+	char choice;
+	cin >> choice;
+	if (choice.toupper())*/
+
+	GB game;
+
+	while (game.gameOverCheck()) {
+		//std::cin >> move;
+		game.updateTempGB();
+		//game.setCommand(move);
+		game.changeGB();
+	}
+	std::cout << "Game Over!\nScore: " << game.getScore() << std::endl;
+
+}
+int main() {
+	playGame();
+	return 0;
+}
